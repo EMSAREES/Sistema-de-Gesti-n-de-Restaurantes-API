b6c80a26e149f3fda65637a6de7992ea658341f7
EMSAREES <lobomategamer@gmail.com>
2026-05-13 19:59:42 -0700

    feat: ajustar createRestaurantController para responder con código 202 Accepted y objeto creado

--- src/controller/restaurant.controller.ts
+++ src/controller/restaurant.controller.ts
@@ -6,7 +6,7 @@ export const createRestaurantController = async (req: Request, res: Response) =>
     try {
         const restaurant = await createRestaurant(req.body); // se debe mandar en formato json
 
-        res.status(HTTP_STATUS_CODES.CREATED).json({results: restaurant});
+        res.status(HTTP_STATUS_CODES.CREATED).json({code: HTTP_STATUS_CODES.CREATED, results: restaurant});
     }catch (error){
         const message = error instanceof Error ? error.message : "Error inesperado.";
         res.status(HTTP_STATUS_CODES.SERVICE_UNAVAILABLE).json({ error: message });
