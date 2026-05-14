c59c2f8d728531ae11d55ee7688e3f38f70cf165
EMSAREES <lobomategamer@gmail.com>
2026-05-13 23:43:03 -0700

    chore: ajustar mensaje de inicio del servidor para mostrar ruta base /api en la consola para no solo copiar y pegarlo

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,5 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log(`erver is running at http://localhost:${PORT}`);
+    console.log(`erver is running at http://localhost:${PORT}/api`);
 });
