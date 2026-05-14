f8f0ef3724ec4f94be72b8e0263d6135dd367a52
EMSAREES <lobomategamer@gmail.com>
2026-05-14 12:34:21 -0700

    docs: integrar Swagger UI en /api-docs para visualizar documentación

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,9 @@ import cors from "cors";
 import type { Request, Response } from "express";
 import router from "./router/index.routes";
 
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./swagger/swagger";
+
 // import "./database/db";
 
 
@@ -15,5 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use("/api", router);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 export default app;
