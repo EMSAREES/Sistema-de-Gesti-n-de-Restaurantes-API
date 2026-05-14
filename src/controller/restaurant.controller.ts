ab73ff425c6a5e2922e48383038a13c810647adc
EMSAREES <lobomategamer@gmail.com>
2026-05-13 20:32:50 -0700

    feat: agregar función getRestaurants en controlador para listar restaurantes con filtros opcionales city e isActive

--- src/controller/restaurant.controller.ts
+++ src/controller/restaurant.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { createRestaurant } from "../service/restaurant.service";
+import { createRestaurant, getRestaurants } from "../service/restaurant.service";
 import { HTTP_STATUS_CODES } from "../constants/stateCodes";
 
 export const createRestaurantController = async (req: Request, res: Response) => {
@@ -12,3 +12,29 @@ export const createRestaurantController = async (req: Request, res: Response) =>
         res.status(HTTP_STATUS_CODES.SERVICE_UNAVAILABLE).json({ error: message });
     }
 }
+
+export const getRestaurantsController = async (req: Request, res: Response) => {
+    try {
+        const { city, isActive } = req.query;
+
+        // convertir isActive a booleano esta fue la solucion que encontre
+        const parsedIsActive =
+            isActive === "true"
+                ? true
+                : isActive === "false"
+                ? false
+                : undefined;
+        
+        const restaurants = await getRestaurants(
+            city as string ,
+            isActive as boolean | undefined
+        );
+
+        res.status(HTTP_STATUS_CODES.OK).json({ restaurants });
+    } catch (error) {
+        const message = error instanceof Error ? error.message : "Error inesperado.";
+        res
+        .status(HTTP_STATUS_CODES.SERVICE_UNAVAILABLE)
+        .json({ error: message });
+    }
+};
