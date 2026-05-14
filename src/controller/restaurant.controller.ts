ab00245f7f23b7c810c5300eb42b16c7a7fbaa2a
EMSAREES <lobomategamer@gmail.com>
2026-05-13 19:52:51 -0700

    feat: implementar controlador createRestaurant con manejo de errores y respuesta estandarizada

---
+++ src/controller/restaurant.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { createRestaurant } from "../service/restaurant.service";
+import { HTTP_STATUS_CODES } from "../constants/stateCodes";
+
+export const createRestaurantController = async (req: Request, res: Response) => {
+    try {
+        const restaurant = await createRestaurant(req.body); // se debe mandar en formato json
+
+        res.status(HTTP_STATUS_CODES.CREATED).json({results: restaurant});
+    }catch (error){
+        const message = error instanceof Error ? error.message : "Error inesperado.";
+        res.status(HTTP_STATUS_CODES.SERVICE_UNAVAILABLE).json({ error: message });
+    }
+}
